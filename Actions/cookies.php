25f8ce73b9e88c5c1018109e3664f9cb05f11d18
Javier Gomez Pazo <javiergomezpazo>
2020-03-24 22:34:59 +0100

    Added documentation

--- Actions/cookies.php
+++ Actions/cookies.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * This file contains common functions used throughout the application.
+ *
+ * @package    Actions
+ * @author     Javier Gomez <gomez.javiergomez11@gmail.com>
+ */
 
+/**
+ * Validate if the user have close banner cookies
+ *
+ * Return between AJAX, true or false
+ * 
+ */
 function comprobarCookieBanner(){
     if (isset($_COOKIE['bannerCookies']) or comprobarSession()) {
         echo "true";
@@ -8,6 +20,10 @@ function comprobarCookieBanner(){
     }
 }
 
+/**
+ * Create cookies "bannerCookies"
+ * 
+ */
 function cerrarBanner(){
     if (!isset($_COOKIE['bannerCookies'])){
         setcookie('bannerCookies', "Banner Close", time() + 3600 * 80);
